87989d7c1ba7844bc31084b23d98fdfb589b6973
MHHsim <marta.harana@gmail.com>
2026-03-30 19:46:47 +0200

    Update clases.py

---
+++ clases.py
@@ -0,0 +1,80 @@
+import numpy as np
+import random
+from variables import (
+    board_size,
+    barcos,
+    symbol_agua,
+    symbol_barco,
+    symbol_disparo,
+    symbol_fallo,
+    orientations
+)
+
+class Tablero:
+
+    def __init__(self, player_id: str):
+        """Inicializa el tablero del jugador."""
+        self.player_id = player_id
+        self.size = board_size
+        self.board = np.full((self.size, self.size), symbol_agua)
+        self.tracking = np.full((self.size, self.size), symbol_agua)
+
+    def colocar_barco(self, tipo_barco, fila, columna, orientacion):
+        """
+        Coloca un barco en la posición indicada con orientación N, S, E u O.
+        Devuelve True si se colocó correctamente, False si no cabe o choca.
+        """
+
+        eslora = barcos[tipo_barco]["eslora"]
+
+        # --- 1. Comprobar si cabe dentro del tablero ---
+        if orientacion == "N":
+            if fila - (eslora - 1) < 0:                   # Para Norte y Sur, la columna no cambia, por eso se comprueba
+                return False                                # que la fila esté en el rango eslora - 1
+        elif orientacion == "S":                            # En Este y Oeste, lo que se comprueba es que la columna,
+            if fila + (eslora - 1) >= self.size:            # sea menor que la matriz (self.size)
+                return False
+        elif orientacion == "E":
+            if columna + (eslora - 1) >= self.size:
+                return False
+        elif orientacion == "O":
+            if columna - (eslora - 1) < 0:
+                return False
+
+        # --- 2. Comprobar que no pisa otro barco ---
+        posiciones = []
+
+        for i in range(eslora):
+            if orientacion == "N":
+                f, c = fila - i, columna         # Para N y S, la columna no cambia, solo la fila
+            elif orientacion == "S":                # Para E y O, la fila no cambia, solo la columna
+                f, c = fila + i, columna
+            elif orientacion == "E":
+                f, c = fila, columna + i
+            elif orientacion == "O":
+                f, c = fila, columna - i
+
+            if self.board[f, c] == symbol_barco:
+                return False
+
+            posiciones.append((f, c)) # se guardan las posiciones si todas cumplen. Una vez guardadas, coloca el barco
+
+        # --- 3. Colocar el barco ---
+        for f, c in posiciones:
+            self.board[f, c] = symbol_barco
+        
+        # --- 4. Imprimir el tablero después de colocar ---
+        print(self.board)
+
+
+        return True
+
+    
+    def recibir_disparo(self, fila, columna):
+        if fila, columna == symbol_agua:
+            return "Agua"
+        
+        elif fila, columna == symbol_barco:
+            return "Tocado"
+        
+        elif fila, columna == 
